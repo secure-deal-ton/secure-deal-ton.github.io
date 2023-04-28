756a013984f3276a2256ab0a97d46ea75f1c24a3
Vladislav Orlov <me@orlov-vo.ru>
2023-04-28 11:30:11 +0300

    change out dir to `/dist`

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     root: 'web',
     plugins: [react()],
+    build: { outDir: path.resolve(__dirname, 'dist') },
 });
